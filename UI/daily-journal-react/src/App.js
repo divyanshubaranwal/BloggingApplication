9ddd52632fa16a3d4bf7c80f163d47dab104d1a7
Divyanshu <divyanshu9210@gmail.com>
2021-03-17 16:41:36 +0530

    added context api

--- UI/daily-journal-react/src/App.js
+++ UI/daily-journal-react/src/App.js
@@ -6,19 +6,22 @@ import Contact from "./components/contact";
 import Compose from "./components/compose";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Post from "./components/post";
+import BlogProvider from "../src/contexts/blogContext";
 
 function App() {
   return (
     <Router>
       <React.Fragment>
         <Navbar />
-        <div className="container">
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/compose" component={Compose} />
-          <Route path="/posts/:id" component={Post} />
-        </div>
+        <BlogProvider>
+          <div className="container">
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/compose" component={Compose} />
+            <Route path="/posts/:id" component={Post} />
+          </div>
+        </BlogProvider>
       </React.Fragment>
     </Router>
   );
